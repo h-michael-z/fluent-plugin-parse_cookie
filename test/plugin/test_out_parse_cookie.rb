5947e46c01bdefb1579ac501448824fd914b452e
Hirokazu Hata <h.hata.ai.t@gmail.com>
2018-11-20 12:36:48 +0900

    Add rubocop for dev dependency

--- test/plugin/test_out_parse_cookie.rb
+++ test/plugin/test_out_parse_cookie.rb
@@ -1,20 +1,13 @@
 require 'helper'
 require 'rr'
-require 'timecop'
 require 'pry-byebug'
 require 'fluent/plugin/out_parse_cookie'
 
 class ParseCookieOutputTest < Test::Unit::TestCase
-
-  COOKIE = 'temporary=tmp; empty=; __test=miahcel; array=123; array=abc; array=1a2b'
+  COOKIE = 'temporary=tmp; empty=; __test=miahcel; array=123; array=abc; array=1a2b'.freeze
 
   def setup
     Fluent::Test.setup
-    Timecop.freeze(@time)
-  end
-
-  teardown do
-    Timecop.return
   end
 
   def create_driver(conf, tag)
@@ -23,16 +16,16 @@ class ParseCookieOutputTest < Test::Unit::TestCase
     ).configure(conf)
   end
 
-  def emit(conf, record, tag='test')
+  def emit(conf, record, tag = 'test')
     d = create_driver(conf, tag)
-    d.run {d.emit(record)}
-    emits = d.emits
+    d.run { d.emit(record) }
+    d.emits
   end
 
   def test_configure
-    d = create_driver(%[
+    d = create_driver(%(
       key                cookie
-    ], "test")
+    ), 'test')
 
     assert_equal 'cookie',          d.instance.key
     assert_equal 'parsed_cookie.',  d.instance.tag_prefix
@@ -40,158 +33,158 @@ class ParseCookieOutputTest < Test::Unit::TestCase
   end
 
   def test_parse_cookie
-    conf = %[
+    conf = %(
       key            cookie
-    ]
+    )
 
     record = {
-      'cookie' => COOKIE,
+      'cookie' => COOKIE
     }
 
     emits = emit(conf, record)
 
-    emits.each_with_index do |(tag, time, record), i|
+    emits.each_with_index do |(tag, _time, emit_record), _i|
       assert_equal 'parsed_cookie.test',                     tag
-      assert_equal ['tmp'],                                  record['temporary']
-      assert_equal [],                                       record['empty']
-      assert_equal ['miahcel'],                              record['__test']
-      assert_equal ['123', 'abc', '1a2b'],                   record['array']
-      assert_equal COOKIE,                                   record['cookie']
+      assert_equal ['tmp'],                                  emit_record['temporary']
+      assert_equal [],                                       emit_record['empty']
+      assert_equal ['miahcel'],                              emit_record['__test']
+      assert_equal %w[123 abc 1a2b], emit_record['array']
+      assert_equal COOKIE, emit_record['cookie']
     end
   end
 
   def test_delete_cookie
-    conf = %[
+    conf = %(
       key            cookie
       remove_cookie  true
-    ]
+    )
 
     record = {
-      'cookie' => COOKIE,
+      'cookie' => COOKIE
     }
 
     emits = emit(conf, record)
 
-    emits.each_with_index do |(tag, time, record), i|
-      assert_equal 'parsed_cookie.test',              tag
-      assert_equal ['tmp'],       record['temporary']
-      assert_equal [],            record['empty']
-      assert_equal ['miahcel'],     record['__test']
-      assert_equal ['123', 'abc', '1a2b'],     record['array']
-      assert_equal nil,     record['cookie']
+    emits.each_with_index do |(tag, _time, emit_record), _i|
+      assert_equal 'parsed_cookie.test', tag
+      assert_equal ['tmp'],       emit_record['temporary']
+      assert_equal [],            emit_record['empty']
+      assert_equal ['miahcel'], emit_record['__test']
+      assert_equal %w[123 abc 1a2b], emit_record['array']
+      assert_equal nil, emit_record['cookie']
     end
   end
 
   def test_add_tag_prefix
-    conf = %[
+    conf = %(
       key            cookie
       tag_prefix     add_tag.
-    ]
+    )
 
     record = {
-      'cookie' => COOKIE,
+      'cookie' => COOKIE
     }
 
     emits = emit(conf, record)
 
-    emits.each_with_index do |(tag, time, record), i|
-      assert_equal 'add_tag.test',              tag
-      assert_equal ['tmp'],       record['temporary']
-      assert_equal [],            record['empty']
-      assert_equal ['miahcel'],     record['__test']
-      assert_equal ['123', 'abc', '1a2b'],     record['array']
-      assert_equal COOKIE,     record['cookie']
+    emits.each_with_index do |(tag, _time, emit_record), _i|
+      assert_equal 'add_tag.test', tag
+      assert_equal ['tmp'],       emit_record['temporary']
+      assert_equal [],            emit_record['empty']
+      assert_equal ['miahcel'], emit_record['__test']
+      assert_equal %w[123 abc 1a2b], emit_record['array']
+      assert_equal COOKIE, emit_record['cookie']
     end
   end
 
   def test_remove_empty_array
-    conf = %[
+    conf = %(
       key                 cookie
       remove_empty_array  true
-    ]
+    )
 
     record = {
-      'cookie' => COOKIE,
+      'cookie' => COOKIE
     }
 
     emits = emit(conf, record)
 
-    emits.each_with_index do |(tag, time, record), i|
-      assert_equal 'parsed_cookie.test',              tag
-      assert_equal ['tmp'],       record['temporary']
-      assert_equal nil,            record['empty']
-      assert_equal ['miahcel'],     record['__test']
-      assert_equal ['123', 'abc', '1a2b'],     record['array']
-      assert_equal COOKIE,     record['cookie']
+    emits.each_with_index do |(tag, _time, emit_record), _i|
+      assert_equal 'parsed_cookie.test', tag
+      assert_equal ['tmp'], emit_record['temporary']
+      assert_equal nil, emit_record['empty']
+      assert_equal ['miahcel'], emit_record['__test']
+      assert_equal %w[123 abc 1a2b], emit_record['array']
+      assert_equal COOKIE, emit_record['cookie']
     end
   end
 
   def test_single_value_to_string
-    conf = %[
+    conf = %(
       key                     cookie
       single_value_to_string  true
-    ]
+    )
 
     record = {
-      'cookie' => COOKIE,
+      'cookie' => COOKIE
     }
 
     emits = emit(conf, record)
 
-    emits.each_with_index do |(tag, time, record), i|
-      assert_equal 'parsed_cookie.test',              tag
-      assert_equal 'tmp',       record['temporary']
-      assert_equal [],            record['empty']
-      assert_equal 'miahcel',     record['__test']
-      assert_equal ['123', 'abc', '1a2b'],     record['array']
-      assert_equal COOKIE,     record['cookie']
+    emits.each_with_index do |(tag, _time, emit_record), _i|
+      assert_equal 'parsed_cookie.test', tag
+      assert_equal 'tmp', emit_record['temporary']
+      assert_equal [],            emit_record['empty']
+      assert_equal 'miahcel',     emit_record['__test']
+      assert_equal %w[123 abc 1a2b], emit_record['array']
+      assert_equal COOKIE, emit_record['cookie']
     end
   end
 
   def test_remove_empty_array_single_value_to_string
-    conf = %[
+    conf = %(
       key                     cookie
       remove_empty_array      true
       single_value_to_string  true
-    ]
+    )
 
     record = {
-      'cookie' => COOKIE,
+      'cookie' => COOKIE
     }
 
     emits = emit(conf, record)
 
-    emits.each_with_index do |(tag, time, record), i|
-      assert_equal 'parsed_cookie.test',              tag
-      assert_equal 'tmp',       record['temporary']
-      assert_equal nil,            record['empty']
-      assert_equal 'miahcel',     record['__test']
-      assert_equal ['123', 'abc', '1a2b'],     record['array']
-      assert_equal COOKIE,     record['cookie']
+    emits.each_with_index do |(tag, _time, emit_record), _i|
+      assert_equal 'parsed_cookie.test', tag
+      assert_equal 'tmp', emit_record['temporary']
+      assert_equal nil, emit_record['empty']
+      assert_equal 'miahcel', emit_record['__test']
+      assert_equal %w[123 abc 1a2b], emit_record['array']
+      assert_equal COOKIE, emit_record['cookie']
     end
   end
 
   def test_sub_key
-    conf = %[
+    conf = %(
       key                     cookie
       sub_key                 cookie_parsed
       remove_empty_array      true
       single_value_to_string  true
-    ]
+    )
 
     record = {
-      'cookie' => COOKIE,
+      'cookie' => COOKIE
     }
 
     emits = emit(conf, record)
 
-    emits.each_with_index do |(tag, time, record), i|
-      assert_equal 'parsed_cookie.test',              tag
-      assert_equal 'tmp',       record['cookie_parsed']['temporary']
-      assert_equal nil,            record['cookie_parsed']['empty']
-      assert_equal 'miahcel',     record['cookie_parsed']['__test']
-      assert_equal ['123', 'abc', '1a2b'],     record['cookie_parsed']['array']
-      assert_equal COOKIE,     record['cookie']
+    emits.each_with_index do |(tag, _time, emit_record), _i|
+      assert_equal 'parsed_cookie.test', tag
+      assert_equal 'tmp', emit_record['cookie_parsed']['temporary']
+      assert_equal nil, emit_record['cookie_parsed']['empty']
+      assert_equal 'miahcel', emit_record['cookie_parsed']['__test']
+      assert_equal %w[123 abc 1a2b], emit_record['cookie_parsed']['array']
+      assert_equal COOKIE, emit_record['cookie']
     end
   end
 end
